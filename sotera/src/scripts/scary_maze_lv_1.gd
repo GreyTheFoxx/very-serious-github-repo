7b3945c77d30a49b5a8da0b20dcc973dcb292f4d
WhimFreak <whimmyfreaky@gmail.com>
2026-06-27 00:34:01 +0800

    Changed scary maze jumpscare condition to a timer

--- sotera/src/scripts/scary_maze_lv_1.gd
+++ sotera/src/scripts/scary_maze_lv_1.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 @export var point_light: PointLight2D
 @export var darkness: CanvasModulate
-@onready var player: Player = $"../Player2"
+@export var visual_timer_anim: AnimationPlayer
 
+@onready var player: Player = $"../Player2"
 
 @onready var hp = 3
 
@@ -18,11 +19,24 @@ var jumpscare_active: bool = false
 func _ready() -> void:
 	MusicPlayer.play_track(MusicPlayer.SCARY, 0.5, 0.0, -3.0)
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+	# Stops anim when contract is collected
+	Events.collect_contract.connect(func(): visual_timer_anim.stop())
+	
+	visual_timer_anim.animation_finished.connect(
+		func(anim_name: StringName):
+			if anim_name == &"gradual_dim":
+				trigger_jumpscare()
+			)
+	visual_timer_anim.play("gradual_dim")
 
 func _exit_tree() -> void:
 	MusicPlayer.stop_track(2.0)
 
-func _on_exit_area_body_entered(body: CharacterBody2D) -> void:
+#func _on_exit_area_body_entered(body: CharacterBody2D) -> void:
+	#trigger_jumpscare()
+	
+func trigger_jumpscare():
 	if jumpscare_active:
 		return
 		
@@ -36,7 +50,10 @@ func _on_exit_area_body_entered(body: CharacterBody2D) -> void:
 	# Disable darkness and trigger jumpscare
 	point_light.visible = false
 	darkness.visible = false
-	
+	 
+	# Center jumpscare scene to player and adjust it a bit to cover the whole screen
+	# Probably not the best approach lol
+	jumpscare.global_position = player.global_position + Vector2(100, 350)
 	jumpscare.start_jumpscare()
 	
 	# Wait till jumpscare is done
@@ -44,7 +61,7 @@ func _on_exit_area_body_entered(body: CharacterBody2D) -> void:
 	jumpscare.voice.stop()
 	
 	# Static
-	static_screen.global_position = player.global_position * static_screen.pivot_offset_ratio
+	static_screen.global_position = player.global_position - (static_screen.size / 2)
 	static_anim.play("play_static")
 	await static_anim.animation_finished
 	
@@ -60,6 +77,7 @@ func _on_exit_area_body_entered(body: CharacterBody2D) -> void:
 	darkness.show()
 	static_anim.play("RESET")
 	timer.start()
+	visual_timer_anim.play("gradual_dim")
 
-func _on_timer_timeout() -> void:
+func _on_timer_timeout() -> void: # Hacky fix to prevent jumpscare triggering twice
 	jumpscare_active = false
