e77fa5816c2c36988388b35f866b65f9f43a7824
[Code] : Programmer Lead <56315507+Alestesa@users.noreply.github.com>
2026-06-26 20:52:39 +0200

    Pause Menu
    Added pause menu (fiknda, its fake pasue menu -> palyer visaul see pause) when toggleing curatins between open/closed. Closed means paused.

--- sotera/src/scripts/wheel/FortuneWheelCurtainsClick.gd
+++ sotera/src/scripts/wheel/FortuneWheelCurtainsClick.gd
@@ -2,18 +2,37 @@ extends Area2D
 
 @export var curtains: CurtainSystem
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion: return # ignore mouse motion
-	print("HHJ")
-	if event.pressed: return # touch up is required
-	
-	# Mouse click
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		_curtain_body_clicked()
-		
-	# Mobile touch
-	elif event is InputEventScreenTouch: _curtain_body_clicked()
+var index: int = -1
 
-func _curtain_body_clicked() -> void:
+func _input(e: InputEvent) -> void:
+	if !curtains.closed(): return
+
+	if (e is InputEventScreenTouch or e is InputEventMouseButton) and !e.pressed:
+		force_touch_up()
+
+# pressing polying inside world
+func _input_event(viewport: Viewport, e: InputEvent, shape_idx: int) -> void:
+	if   e is InputEventScreenTouch: touch_down_up_action(e, e.index)
+	elif e is InputEventMouseButton: touch_down_up_action(e, 0)
+
+func touch_down_up_action(event: InputEvent, index: int) -> void:
+	if event.pressed: touch_down(index)
+	else: touch_up(index)
+
+func force_touch_up() -> void:
+	_perform_click()
+	self.index = -1
+	
+func touch_up(index: int) -> void:
+	if index != self.index: return
+	
+	_perform_click()
+	self.index = -1
+	
+func touch_down(index: int) -> void:
+	if self.index != -1: return
+	self.index = index
+
+func _perform_click() -> void:
 	if curtains.closed(): curtains.open_full()
 	elif curtains.opened(): curtains.close_full()
---
+++ sotera/src/scripts/wheel/FortuneWheelPauseUi.gd
@@ -0,0 +1,69 @@
+extends SpaceReturn
+class_name PauseBox
+
+enum PauseBoxState{ HIDDEN, APPEARING, SHOWN, DISAPPEARING }
+@export var range_full_appear_visibility_time: Vector2 = Vector2(0.5, 1.5)
+@export var curtains: CurtainSystem
+
+@onready var pause = $Pause
+@onready var space_return = $"Space To Return"
+
+var _state: PauseBoxState = PauseBoxState.HIDDEN
+var _time: float = 0.0
+var _max_time: float
+
+func _ready() -> void:
+	curtains.on_close.connect(show_pause)
+	curtains.on_opening.connect(hide_pause)
+	
+	# auto hide ... enabled here cause Editor visibility
+	pause.modulate.a = 0.0
+	space_return.modulate.a = 0.0
+	
+func _process(delta: float) -> void:
+	match _state:
+		PauseBoxState.APPEARING: _update_appearing(delta)
+		PauseBoxState.DISAPPEARING: _update_disapeparing(delta)
+
+func _update_appearing(delta: float) -> void:
+	_time = min(_time + delta, _max_time)
+	if _time == _max_time: _state = PauseBoxState.SHOWN
+	
+	_update_visibility()
+	
+func _update_disapeparing(delta: float) -> void:
+	_time = max(_time - delta, 0.0)
+	if _time == 0.0: _state = PauseBoxState.HIDDEN
+	
+	_update_visibility()
+	
+func _update_visibility() -> void:
+	var t: float = _time / _max_time
+	var alpha: float = TweenUtils.ease_out_quart(t)
+	
+	var opacity: float = alpha
+
+	
+	pause.modulate.a = opacity
+	space_return.modulate.a = opacity
+	
+func show_pause() -> void:
+	var skip: bool = _state == PauseBoxState.APPEARING || _state == PauseBoxState.SHOWN
+	if skip: return
+	
+	_time = 0.0
+	_max_time = randf_range(range_full_appear_visibility_time.x, range_full_appear_visibility_time.y)
+	
+	_state = PauseBoxState.APPEARING
+
+# override default behavior of super class
+func action() -> void:
+	# action_open_curtains()
+	if curtains.closed(): curtains.open_full()
+
+func hide_pause() -> void:
+	var skip: bool = _state == PauseBoxState.DISAPPEARING || _state == PauseBoxState.HIDDEN
+	if skip: return
+	
+	_state = PauseBoxState.DISAPPEARING
+	
